8561d5c1f0b987a6b00440dab141e0a686a868dd
Ricardo Oliveira <ricvenicius@gmail.com>
2025-01-17 19:56:13 -0300

    feat: implement SignIn and SignUp Pages

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpPage } from "@/components/Pages/SignUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Registrar-se"
+}
+
+const SignUp = () => <SignUpPage />
+export default SignUp;
